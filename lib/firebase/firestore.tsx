e24f7ea216076f609c2a279893c479019f5e87eb
Federico0284364 <tabella.federico@gmail.com>
2025-09-20 20:18:53 +0200

    added client_id during google login

--- lib/firebase/firestore.tsx
+++ lib/firebase/firestore.tsx
@@ -1,23 +1,29 @@
 // firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import {
+	getAuth,
+	GoogleAuthProvider,
+	signInWithPopup,
+	signOut,
+} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrkQW2qg7s6XVayHq4FdqmO4ZZf8o3FJ0",
-  authDomain: "album-tracker-30dfc.firebaseapp.com",
-  projectId: "album-tracker-30dfc",
-  storageBucket: "album-tracker-30dfc.appspot.com",
-  messagingSenderId: "462568715743",
-  appId: "1:462568715743:web:1736b71e6542f2ed42406a",
-  measurementId: "G-2DCL5KKTXM",
+	apiKey: "AIzaSyDrkQW2qg7s6XVayHq4FdqmO4ZZf8o3FJ0",
+	authDomain: "album-tracker-30dfc.firebaseapp.com",
+	projectId: "album-tracker-30dfc",
+	storageBucket: "album-tracker-30dfc.appspot.com",
+	messagingSenderId: "462568715743",
+	appId: "1:462568715743:web:1736b71e6542f2ed42406a",
+	measurementId: "G-2DCL5KKTXM",
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-  prompt: "select_account",
+	prompt: "select_account",
+	client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string, // <- qui usi la variabile
 });
 const db = getFirestore(app);
 
