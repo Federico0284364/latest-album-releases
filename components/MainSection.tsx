bc13bfebaf9c270c1565796d1f7608b3acfe0a3c
Federico0284364 <tabella.federico@gmail.com>
2025-09-25 11:12:59 +0200

    test cron job 3

--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -87,6 +87,7 @@ export default function MainSection() {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
+				"Authorization": `Bearer ${process.env.NEXT_PUBLIC_CRON_SECRET}`
 			},
 		});
 
